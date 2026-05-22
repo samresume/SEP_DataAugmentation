fdba1e6499c81c7e360863b2e788f2dc7f1828ae
Garrett Woodhouse <garrettwoodhouse@gmail.com>
2026-05-22 14:40:03 -0600

    Adding dependancies runnable

---
+++ 0_Dependancies.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Install Dependancies
+# 
+
+# In[ ]:
+
+
+get_ipython().system('pip install pandas numpy matplotlib seaborn tqdm scikit-learn scipy imbalanced-learn tensorflow sktime')
+
